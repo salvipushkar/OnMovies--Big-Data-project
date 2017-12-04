10a7e7006d0877a03b6d90c532f816822da05427
salvipushkar <30918081+salvipushkar@users.noreply.github.com>
2017-12-04 00:19:14 -0500

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -65,4 +65,57 @@ router.get('/search',function(req,response,next){
     });
 });
 
+
+// redis
+var redis = require('redis');
+
+var redisclient = redis.createClient(6379,rediscluster.xt7pjf.0001.use2.cache.amazonaws.com);
+
+//var redisclient = redis.createClient();
+redisclient.auth('password', function (err) {
+    if (err) throw err;
+});
+
+redisclient.on('connect', function() {
+    console.log('Connected to Redis');
+});
+
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+    redisclient.get("Recommended",function(err,result){
+        var c1 = [];
+        var chunkSize =4;
+        var obj1 = JSON.parse(result.replace(/'/g, '"'));
+
+        for(var i=0;i<obj1.length;i+=chunkSize){
+            if(i==0)
+                c1.push(obj1.slice(i,i+chunkSize));
+            
+            //console.log(productChunks);
+        }
+        redisclient.get("PopularThisWeek",function(err,result){
+            var t1 = [];
+            var chunkSize =4;
+            var obj2 = JSON.parse(result.replace(/'/g, '"'));
+
+            for(var i=0;i<obj2.length;i+=chunkSize){
+                if(i==0)
+                    t1.push(obj2.slice(i,i+chunkSize));
+                
+                //console.log(productChunks);
+            }
+            
+
+                
+                res.render('shop/indexMain', { title: 'Best Movies'
+                    , c1: c1, t1: t1, csrfToken: req.csrfToken()});
+
+                //redisclient.quit();
+            });
+        });
+    });
+});
 module.exports = router;
